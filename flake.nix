af6aced025dda13a095aca205dd207ad9192ec01
shinokun <hypatia.tile02021@gmail.com>
2026-05-05 14:40:10 +0900

    add cabal init script

--- flake.nix
+++ flake.nix
@@ -40,6 +40,27 @@
             echo "ghc=$(ghc --numeric-version),"
             echo "cabal=$(cabal --numeric-version),"
             echo "Try: cabal repl | ghci | ghcid --command 'cabal repl'"
+
+            cabal-init() {
+              if [ $# -ne 1 ]; then
+                echo "Usage: cabal-init <project-name>"
+                return 1
+              fi
+              local project_name=$1
+              cabal init --non-interactive \
+                --package-name="$project_name" \
+                --license=BSD-3-Clause \
+                --libandexe \
+                --tests \
+                --main-is=Main.hs \
+                --language=GHC2021
+              local cabal_file
+              cabal_file=$(find . -maxdepth 1 -name "*.cabal" | head -1)
+              if [ -n "$cabal_file" ]; then
+                sed -i '''' 's/^cabal-version:.*$/cabal-version:      3.14/' "$cabal_file"
+                echo "Patched $cabal_file: cabal-version -> 3.14 (HLS compatibility)"
+              fi
+            }
           '';
         };
       }
